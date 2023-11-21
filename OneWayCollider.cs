69a4e8ac23711397772420b5bcd3627a7414455e
hayabusa <s.hashimoto@hs-sh.net>
2023-11-21 10:54:23 +0900

    落ちてくるときだけ有効になるようにした

--- OneWayCollider.cs
+++ OneWayCollider.cs
@@ -7,6 +7,8 @@ public class OneWayCollider : UdonSharpBehaviour
 {
     private BoxCollider _mainCollider;
     private VRCPlayerApi _localPlayer;
+    private float _colliderSize;
+    private float _colliderOffset;
     public void Start()
     {
         _localPlayer = Networking.LocalPlayer;
@@ -19,9 +21,11 @@ public class OneWayCollider : UdonSharpBehaviour
             else
                 trigger = c;
         }
-        trigger.size = new Vector3(_mainCollider.size.x * 1.5f, _mainCollider.size.y * 2f, _mainCollider.size.z * 1.5f);
-        trigger.center = new Vector3(_mainCollider.center.x, _mainCollider.center.y + _mainCollider.size.y, _mainCollider.center.z);
+        trigger.size = new Vector3(_mainCollider.size.x * 1.5f, _mainCollider.size.y * 4f, _mainCollider.size.z * 1.5f);
+        trigger.center = new Vector3(_mainCollider.center.x, _mainCollider.center.y + trigger.size.y / 2 - 0.1f, _mainCollider.center.z);
         _mainCollider.enabled = false;
+        _colliderOffset = _mainCollider.center.y * transform.localScale.y;
+        _colliderSize = _mainCollider.size.y * transform.localScale.y / 2;
     }
     public override void OnPlayerTriggerEnter(VRCPlayerApi player)
     {
@@ -36,24 +40,17 @@ public class OneWayCollider : UdonSharpBehaviour
     private void Check(VRCPlayerApi player)
     {
         if (!player.isLocal || _mainCollider.enabled) return;
-        if (player.GetPosition().y >= transform.position.y - 0.1f)
+        if (player.GetPosition().y >= transform.position.y + _colliderOffset + _colliderSize && _localPlayer.GetVelocity().y <= 0)
         {
             GetComponent<MeshRenderer>().material.color = Color.blue;
             _mainCollider.enabled = true;
             Debug.Log(Time.frameCount + " " + name + " On");
         }
     }
-    private float _lastPos;
     public void Update()
     {
-        if (name == "Test")
-        {
-            var pos = _localPlayer.GetPosition().y;
-            if (Mathf.Round(_lastPos * 100) != Mathf.Round(pos * 100))
-                Debug.Log(Time.frameCount + " " + pos);
-            _lastPos = pos;
-        }
-        if (_mainCollider.enabled && _localPlayer.GetPosition().y < transform.position.y - 0.1)
+        if (!_mainCollider.enabled) return;
+        if (_localPlayer.GetPosition().y < transform.position.y + _colliderOffset - _colliderSize)
         {
             GetComponent<MeshRenderer>().material.color = Color.green;
             _mainCollider.enabled = false;
